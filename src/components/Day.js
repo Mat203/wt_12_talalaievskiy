73462f46e7cf05d3dae1a0c3b553d2d9fa853805
Mat_Tol <matvey.talalaevskiy@gmail.com>
2023-10-22 18:28:51 +0300

    Task selection

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import { useContext } from 'react';
 import Task from './Task';
+import { SelectedDayContext, TasksContext } from './Week';
 
 const Day = ({ day }) => {
+    const { selectedDay, setSelectedDay } = useContext(SelectedDayContext);
+    const { tasks } = useContext(TasksContext);
+
+    const dayTasks = tasks.filter(task => task.day === day);
+    const totalDuration = dayTasks.reduce((total, task) => total + task.duration, 0);
+
+    const handleClick = () => {
+        setSelectedDay(day);
+    };
+
     return (
-        <div className="day-container" style={{ border: '3px dashed #ccc', marginBottom: '20px' }}>
+        <div 
+            className={`day-container ${selectedDay === day ? 'bg-success' : ''}`} 
+            onClick={handleClick}
+            style={{ border: '3px dashed #ccc', marginBottom: '20px', cursor: 'pointer' }}
+        >
             <h2>{day}</h2>
-            {}
-            <Task />
+            <p>Amount of tasks: {dayTasks.length}</p>
+            <p>Total tasks: {totalDuration}</p>
+            {/* Add your tasks here */}
+            {dayTasks.map(task => <Task {...task} />)}
         </div>
     );
 };
--- src/components/Week.js
+++ src/components/Week.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState, createContext } from 'react';
 import Day from './Day';
+import DayExpanded from './DayExpanded';
+
+export const SelectedDayContext = createContext();
+export const TasksContext = createContext();
 
 const Week = () => {
+    const [selectedDay, setSelectedDay] = useState(null);
+    const [tasks, setTasks] = useState([]);
     const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
     return (
-        <div className="week-container">
-            {days.map(day => <Day day={day} />)}
-        </div>
+        <SelectedDayContext.Provider value={{ selectedDay, setSelectedDay }}>
+            <TasksContext.Provider value={{ tasks, setTasks }}>
+                <div className="week-container">
+                    {days.map((day, index) => (
+                        <div key={index}>
+                            <Day day={day} />
+                        </div>
+                    ))}
+                    {selectedDay && <DayExpanded day={selectedDay} />}
+                </div>
+            </TasksContext.Provider>
+        </SelectedDayContext.Provider>
     );
 };
 
